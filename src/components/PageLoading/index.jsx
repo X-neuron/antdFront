c392be69030fbdf02867ec461540eb3df07f4219
50岁的星空 <398570508@qq.com>
2020-05-18 15:24:44 +0800

    Pageloading 文件夹重命名为PageLoading

---
+++ src/components/PageLoading/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Spin } from 'antd';
+
+export default (props) => (
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      alignContent: 'center',
+      margin: 'auto',
+      paddingTop: 100,
+      textAlign: 'center'
+    }}
+  >
+    <Spin size="large" {...props} />
+  </div>
+)
